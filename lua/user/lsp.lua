44dfff4d6ad0d31d98b3903d8d7c65188c9a173d
 Aleksey Panchenko <panchenko.aleksey.a@gmail.com>
2023-06-26 16:47:26 +0300

    Fix cmp auto select

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -42,8 +42,12 @@ cmp.setup({
     {name = 'luasnip', keyword_length = 2},
   },
   mapping = {
+    ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+    ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+    ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+    ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
     -- `Enter` key to confirm completion
-    ['<CR>'] = cmp.mapping.confirm({select = true}),
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
 
     -- Ctrl+Space to trigger completion menu
     ['<C-Space>'] = cmp.mapping.complete(),
