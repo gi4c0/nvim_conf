ba2bc66910232b8b782b609df7125358c9a715fb
 Aleksey Panchenko <panchenko.aleksey.a@gmail.com>
2023-07-26 16:24:21 +0300

    Update dependencies

--- init.lua
+++ init.lua
@@ -4,10 +4,6 @@ require "user.core.keymaps"
 require "user.core.globals"
 
 require "user.setup-plugins"
--- require "user.core.colorscheme"
 
 -- vim.cmd('source $HOME/.config/nvim/lua/user/plugins/coc.vim')
 vim.cmd('source $HOME/.config/nvim/scripts/scripts.vim')
-
--- Custom
--- require "user.plugins.lint-node"
