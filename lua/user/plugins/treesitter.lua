0ceac76ae0ac70e263832f07f6b4061381028ac1
 Aleksey Panchenko <panchenko.aleksey.a@gmail.com>
2023-09-03 10:57:11 +0300

    Add treesitter text objects

--- lua/user/plugins/treesitter.lua
+++ lua/user/plugins/treesitter.lua
@@ -1,6 +1,9 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
+
+  dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' },
+
   config = function()
     local cachePath = "/Users/alekseypanchenko/.local/share/nvim/tree-sitter-cache"
     vim.opt.runtimepath:append(cachePath)
@@ -11,7 +14,7 @@ return {
         "typescript",
         "json",
         "go",
-        -- "lua",
+        "lua",
         "rust",
         "yaml",
         "toml",
@@ -22,6 +25,61 @@ return {
       },     -- one of "all", "language", or a list of languages
       highlight = {
         enable = true,              -- false will disable the whole extension
+      },
+
+      textobjects = {
+        swap = {
+          enable = true,
+          swap_next = {
+            ["<leader>p"] = "@parameter.inner",
+          },
+          swap_previous = {
+            ["<leader>P"] = "@parameter.inner",
+          },
+        },
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            -- You can use the capture groups defined in textobjects.scm
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            -- You can optionally set descriptions to the mappings (used in the desc parameter of
+            -- nvim_buf_set_keymap) which plugins like which-key display
+            ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+            -- You can also use captures from other query groups like `locals.scm`
+            ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+          },
+        },
+
+        move = {
+          enable = true,
+          set_jumps = true, -- whether to set jumps in the jumplist
+          goto_next_start = {
+            ["]f"] = "@function.outer",
+            ["]r"] = "@return.inner",
+          },
+          goto_next_end = {
+            ["]F"] = "@function.outer",
+          },
+          goto_previous_start = {
+            ["[f"] = "@function.outer",
+            ["[r"] = "@return.outer",
+          },
+          goto_previous_end = {
+            ["[F"] = "@function.outer",
+          },
+          -- Below will go to either the start or the end, whichever is closer.
+          -- Use if you want more granular movements
+          -- Make it even more gradual by adding multiple queries and regex.
+          goto_next = {
+            ["]i"] = "@conditional.outer",
+          },
+          goto_previous = {
+            ["[i"] = "@conditional.outer",
+          }
+        },
       }
     }
   end
