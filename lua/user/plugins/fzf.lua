63eb876882b012b030d4f43927aa693b72a3d127
Oleksii Panchenko <panchenko.o@brainrocket.com>
2024-12-19 14:55:20 +0000

    Ajust fzf lua

--- lua/user/plugins/fzf.lua
+++ lua/user/plugins/fzf.lua
@@ -15,6 +15,14 @@ return {
     require("fzf-lua").setup({
       'telescope',
       fzf_opts   = { ["--layout"] = "default", ["--marker"] = "+" },
+      oldfiles = {
+          include_current_session = true,
+      },
+      previewers = {
+          builtin = {
+              syntax_limit_b = 1024 * 300, -- 300KB
+          }
+      },
       winopts    = {
         width   = 0.8,
         height  = 0.9,
@@ -35,8 +43,9 @@ return {
     -- {'<leader>fr', ':FzfLua oldfiles<CR>', noremap = true, silent = true, "Recent files" },
     {'<leader>gB', ':FzfLua git_branches<CR>', noremap = true, silent = true, "Git branches" },
     {'<C-Space>', "<cmd>lua require('fzf-lua').buffers()<CR>", silent = true},
-    -- {'<leader>/', ":FzfLua live_grep<CR>", silent = true},
+    {'<leader>/', ":FzfLua live_grep<CR>", silent = true},
     {'<leader>*', ":FzfLua grep_cword<CR>", silent = true, mode = {'n'}},
+    {'gR', ":FzfLua grep_cword<CR> resolver", silent = true, mode = {'n'}, desc = "Grep in Resolvers"},
     {'<leader>*', ":FzfLua grep_visual<CR>", silent = true, mode = {'v'}},
     {'<leader>ss', ":FzfLua grep<CR>", silent = true},
     {'<leader>sl', ":FzfLua grep_last<CR>", silent = true},
